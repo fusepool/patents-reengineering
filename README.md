5513c3fa00c880a3d26ab1973e7bde4d5b45dea2
Sarven Capadisli <info@csarven.ca>
2013-01-31 08:35:38 +0100

    Updated README to reflect the removal of config.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ XSLT 2.0 templates to transform MAREC XML to RDF/XML.
 
 ## Requirements
 
-An XSLT 2.0 processor to transform, and some configuring using the provided config.rdf file.
+An XSLT 2.0 processor to transform, and some configuring to change defaults.
 
 ## What can it do?
 Currently it handles bibliogrpahic-data: publication and application references, priority-claims, technical-data (classifications), parties (applicants, inventors, assignees).
@@ -20,6 +20,6 @@ The `scripts/` directory contains XSLT 2.0 templates and a simple Bash script to
 There is some sample data under `data/`.
 
 ## How-to
-Either use the provided Bash script for sample data, or take `marec.xsl`, `common.xsl`, and `config.rdf` to place for your application. Run marec.xsl (it imports `common.xsl` and `config.rdf`) and make sure to pass in parameter `xmlDocument=path/to/source/patent-document.xml`.
+Either use the provided Bash script for sample data, or take `marec.xsl`, `common.xsl` to your application. Run marec.xsl (it imports `common.xsl`).
 
-If you want to change the config, either edit `config.rdf` in place, or use `config.ttl` for a simplicty, but make sure to generate an abbreviated RDF/XML. It is used by XSL to grab some of the values. Don't touch `marec.xsl` or `common.xsl` unless you are developing!
+If you want to change the config, edit `common.xsl` directly becauase apparently having a config in the first place was a bit too confusing for some scientist / engineer extraordinaires - pretty standard practice to have config but away it went (see commit 99f479aa249728acb9aa594614a608d394b22d5b).
--- scripts/marec.sh
+++ scripts/marec.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-rapper -g config.ttl -o rdfxml-abbrev > config.rdf
-
-#http://www.ifs.tuwien.ac.at/imp/marec.shtml
-
 #ls -1 ../data/*.xml | sed 's/\.xml//' | while read i; do saxonb-xslt -t -tree:linked -s "$i".xml -xsl generic.xsl > "$i".rdf; echo "Created $i.rdf"; done
-
 #for i in MAREC/*/*/*.xml ; do filename=$(basename $i) ; path=${i%/*} ; xmllint --format "$i" > /tmp/"$filename" ; mv /tmp/"$filename" "$path""/""$filename" ; done
-
 #grep -RE "kind=\"[^\"]*\"" MAREC/*/*/*.xml | perl -pe 's/.*kind=\"([^\"]*)\".*/$1/' | sort -u
 
-saxonb-xslt -t -tree:linked -s ../data/00/00/EP-1000000-A1.xml -xsl marec.xsl xmlDocument=../data/00/00/EP-1000000-A1.xml > ../data/00/00/EP-1000000-A1.rdf
+#Data from #http://www.ifs.tuwien.ac.at/imp/marec.shtml
 
-saxonb-xslt -t -tree:linked -s ../data/00/00/EP-1000000-B1.xml -xsl marec.xsl xmlDocument=../data/00/00/EP-1000000-B1.xml > ../data/00/00/EP-1000000-B1.rdf
+#saxonb-xslt -t -tree:linked -s ../data/00/00/EP-1000000-A1.xml -xsl marec.xsl > ../data/00/00/EP-1000000-A1.rdf
 
-saxonb-xslt -t -tree:linked -s ../data/00/06/EP-1000006-A1.xml -xsl marec.xsl xmlDocument=../data/00/06/EP-1000006-A1.xml > ../data/00/06/EP-1000006-A1.rdf
+#saxonb-xslt -t -tree:linked -s ../data/00/00/EP-1000000-B1.xml -xsl marec.xsl > ../data/00/00/EP-1000000-B1.rdf
 
-saxonb-xslt -t -tree:linked -s ../data/00/06/EP-1000006-B1.xml -xsl marec.xsl xmlDocument=../data/00/06/EP-1000006-B1.xml > ../data/00/06/EP-1000006-B1.rdf
+#saxonb-xslt -t -tree:linked -s ../data/00/06/EP-1000006-A1.xml -xsl marec.xsl > ../data/00/06/EP-1000006-A1.rdf
+
+saxonb-xslt -t -tree:linked -s ../data/00/06/EP-1000006-B1.xml -xsl marec.xsl > ../data/00/06/EP-1000006-B1.rdf
