43c5d9c23737616caf7b0bd87b417329030cae2a
Sarven Capadisli <info@csarven.ca>
2013-02-21 10:09:14 +0100

    Added samples from other patent offices.

--- src/main/resources/scripts/marec.sh
+++ src/main/resources/scripts/marec.sh
@@ -6,12 +6,17 @@
 
 #Data from #http://www.ifs.tuwien.ac.at/imp/marec.shtml
 
-saxonb-xslt -ext:on -t -tree:linked -s ../data/marec/00/06/EP-1000006-B1.xml -xsl ../xsl/marec.xsl > ../data/marec/00/06/EP-1000006-B1.rdf
+saxonb-xslt -ext:on -t -tree:linked -s ../data/marec/EP/00/06/EP-1000006-B1.xml -xsl ../xsl/marec.xsl > ../data/marec/EP/00/06/EP-1000006-B1.rdf
 
-saxonb-xslt -ext:on -t -tree:linked -s ../data/marec/00/00/EP-1000000-A1.xml -xsl ../xsl/marec.xsl > ../data/marec/00/00/EP-1000000-A1.rdf
+saxonb-xslt -ext:on -t -tree:linked -s ../data/marec/EP/00/00/EP-1000000-A1.xml -xsl ../xsl/marec.xsl > ../data/marec/EP/00/00/EP-1000000-A1.rdf
 
-saxonb-xslt -ext:on -t -tree:linked -s ../data/marec/00/00/EP-1000000-B1.xml -xsl ../xsl/marec.xsl > ../data/marec/00/00/EP-1000000-B1.rdf
+saxonb-xslt -ext:on -t -tree:linked -s ../data/marec/EP/00/00/EP-1000000-B1.xml -xsl ../xsl/marec.xsl > ../data/marec/EP/00/00/EP-1000000-B1.rdf
 
-saxonb-xslt -ext:on -t -tree:linked -s ../data/marec/00/06/EP-1000006-A1.xml -xsl ../xsl/marec.xsl > ../data/marec/00/06/EP-1000006-A1.rdf
+saxonb-xslt -ext:on -t -tree:linked -s ../data/marec/EP/00/06/EP-1000006-A1.xml -xsl ../xsl/marec.xsl > ../data/marec/EP/00/06/EP-1000006-A1.rdf
 
+saxonb-xslt -ext:on -t -tree:linked -s ../data/marec/US/000000/00/00/H1/US-H1-H.xml -xsl ../xsl/marec.xsl > ../data/marec/US/000000/00/00/H1/US-H1-H.rdf
+
+saxonb-xslt -ext:on -t -tree:linked -s ../data/marec/JP/000010/00/00/01/JP-10000001-A.xml -xsl ../xsl/marec.xsl > ../data/marec/JP/000010/00/00/01/JP-10000001-A.rdf
+
+saxonb-xslt -ext:on -t -tree:linked -s ../data/marec/WO/001978/00/00/01/WO-1978000001-A1.xml -xsl ../xsl/marec.xsl > ../data/marec/WO/001978/00/00/01/WO-1978000001-A1.rdf
 
