fd30329e2171a3ed74cece2efcb61adb8a6ddcad
Sarven Capadisli <info@csarven.ca>
2013-03-12 15:38:56 +0100

    Updated config

--- src/main/resources/scripts/patents.config.sh
+++ src/main/resources/scripts/patents.config.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+namespace="http://fusepool.info/";
 repoMeta="/usr/lib/patents-reengineering/src/main/resources/data/meta/";
 marec="/data/marec/";
+ecla="/data/ecla/";
 tdbAssembler="/usr/lib/fuseki/tdb.patents.ttl"
 JVM_ARGS="-Xmx16000M"
+db="/data/tdb/db/patents";
+javatdbloader="java $JVM_ARGS tdb.tdbloader --desc=$tdbAssembler";
