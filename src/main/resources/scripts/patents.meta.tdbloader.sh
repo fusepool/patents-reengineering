faeab3781825c145a0547d180cb8e05dce6400ab
Sarven Capadisli <info@csarven.ca>
2013-03-12 17:27:43 +0100

    Minor

--- src/main/resources/scripts/patents.meta.tdbloader.sh
+++ src/main/resources/scripts/patents.meta.tdbloader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . ./patents.config.sh
 
-"$javatdbloader" --graph="$namespace"graph/meta "$ecla"import/ecla.nt ;
+java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$ecla"import/ecla.nt ;
 
-for i in "$repoMeta"* ; do "$javatdbloader" --graph="$namespace"graph/meta "$i" ; done
+for i in "$repoMeta"* ; do java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/meta "$i" ; done
