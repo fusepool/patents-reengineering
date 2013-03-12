5d43bf5bbdf0173608621b318c0b8a9244067111
Sarven Capadisli <info@csarven.ca>
2013-03-12 18:05:35 +0100

    Fixed path to db

--- src/main/resources/scripts/patents.config.sh
+++ src/main/resources/scripts/patents.config.sh
@@ -6,5 +6,5 @@ marec="/data/marec/";
 ecla="/data/ecla/";
 tdbAssembler="/usr/lib/fuseki/tdb.patents.ttl";
 JVM_ARGS="-Xmx16000M";
-db="/data/tdb/db/patents";
+db="/data/tdb/db/patents/";
 javatdbloader="java $JVM_ARGS tdb.tdbloader --desc=$tdbAssembler";
