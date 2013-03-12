33da53251ce420301ff23f00382a1ceab43ce9d1
Sarven Capadisli <info@csarven.ca>
2013-03-12 15:20:48 +0100

    Minor

--- src/main/resources/scripts/ecla.create.n-triples.sh
+++ src/main/resources/scripts/ecla.create.n-triples.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 data="/data/ecla/";
 
-for in "$data"*.rdf ; do rapper -g "$i" >> "$data"import/ecla.nt ; done
+for i in "$data"*.rdf ; do rapper -g "$i" >> "$data"import/ecla.nt ; done
