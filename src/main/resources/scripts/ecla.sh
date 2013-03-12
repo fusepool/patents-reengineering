dfb434f519340a1d80baa523b2585ad59b433294
Sarven Capadisli <info@csarven.ca>
2013-03-12 18:40:09 +0100

    Added missing config

--- src/main/resources/scripts/ecla.sh
+++ src/main/resources/scripts/ecla.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+. ./patents.config.sh
+
 for i in "$ecla"*.xml ; do file=$(basename "$i"); fn=${file%.*}; saxonb-xslt -t -tree:linked -s "$i" -xsl "$xslECLA" xmlDocument="$i" > "$ecla""$fn".rdf ; done
