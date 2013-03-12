548b12b0035b8f00b228b973f978a50bd4cfc00a
Sarven Capadisli <info@csarven.ca>
2013-03-12 18:36:25 +0100

    Fixed path to ECLA XSL

--- src/main/resources/scripts/ecla.sh
+++ src/main/resources/scripts/ecla.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-data="/data/ecla/";
-xslDocument="/usr/lib/patents-reengineering/src/main/resources/xsl/ecla.xsl";
-
-for i in "$data"*.xml ; do file=$(basename "$i"); fn=${file%.*}; saxonb-xslt -t -tree:linked -s "$i" -xsl "$xslDocument" xmlDocument="$i" > "$data""$fn".rdf ; done
+for i in "$ecla"*.xml ; do file=$(basename "$i"); fn=${file%.*}; saxonb-xslt -t -tree:linked -s "$i" -xsl "$xslECLA" xmlDocument="$i" > "$ecla""$fn".rdf ; done
--- src/main/resources/scripts/patents.config.sh
+++ src/main/resources/scripts/patents.config.sh
@@ -4,6 +4,7 @@ namespace="http://fusepool.info/";
 repoMeta="/home/fusepool/patents-reengineering/src/main/resources/data/meta/";
 marec="/data/marec/";
 ecla="/data/ecla/";
+xslECLA="/home/fusepool/patents-reengineering/src/main/resources/xsl/ecla.xsl";
 tdbAssembler="/usr/lib/fuseki/tdb.patents.ttl";
 JVM_ARGS="-Xmx16000M";
 db="/data/tdb/db/patents/";
