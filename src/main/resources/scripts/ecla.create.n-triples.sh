0c7d16101a9332dbfa61b8be7d662288c2361c65
Luigi Selmi <luigi@luigi.(none)>
2013-03-22 15:47:20 +0100

    moved folder scripts

--- src/main/resources/scripts/ecla.create.n-triples.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-data="/data/ecla/";
-
-for i in "$data"*.rdf ; do rapper -g "$i" >> "$data"import/ecla.nt ; done
--- src/main/resources/scripts/ecla.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. ./patents.config.sh
-
-for i in "$ecla"*.xml ; do file=$(basename "$i"); fn=${file%.*}; saxonb-xslt -t -tree:linked -s "$i" -xsl "$xslECLA" xmlDocument="$i" > "$ecla""$fn".rdf ; done
