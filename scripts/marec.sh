7e346be67c5f345a87ea7b4079116a7dd79105af
Sarven Capadisli <info@csarven.ca>
2013-03-27 21:09:11 +0100

    Minor

--- scripts/marec.sh
+++ scripts/marec.sh
@@ -3,7 +3,7 @@
 . ./patents.config.sh
 
 marec="/data/IREC/EP/000000/00/";
-marecirdf="/data/marec/";
+marecrdf="/data/marec/";
 
 find "$marec" -name *.xml | while read i ; do path=${i%/*} ; file=$(basename $i) ; fn=${file%.*}; saxonb-xslt -ext:on -tree:linked -s "$i" -xsl ../src/main/resources/xsl/marec.xsl pathToCPCConcordances="$pathToCPConcordances" > "$marecrdf""$fn".rdf ; done
 
