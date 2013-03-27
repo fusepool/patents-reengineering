a9be2449a2c39df015c4d448ecd679dfe0226372
Sarven Capadisli <info@csarven.ca>
2013-03-27 20:36:53 +0100

    Minor

--- scripts/patents.tdbloader.sh
+++ scripts/patents.tdbloader.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 . ./patents.config.sh
 
-find ../data/marec/ -name *.rdf | while read i ; do java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/patents "$i" ; done
+#find ../data/marec/ -name *.rdf | while read i ; do java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/patents "$i" ; done
+
+marec="../data/marec/";
+
+java "$JVM_ARGS" tdb.tdbloader --desc="$tdbAssembler" --graph="$namespace"graph/patents "$marec"import/marec.nt
