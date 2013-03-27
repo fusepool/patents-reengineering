6863fed715b9dda76ee52590ca57403846de4aec
Sarven Capadisli <info@csarven.ca>
2013-03-27 21:06:39 +0100

    A small chunk of marec transformation mostly for testing

--- scripts/marec.sh
+++ scripts/marec.sh
@@ -2,6 +2,12 @@
 
 . ./patents.config.sh
 
+marec="/data/IREC/EP/000000/00/";
+marecirdf="/data/marc/";
+
+find "$marec" -name *.xml | while read i ; do path=${i%/*} ; file=$(basename $i) ; fn=${file%.*}; saxonb-xslt -ext:on -tree:linked -s "$i" -xsl ../src/main/resources/xsl/marec.xsl pathToCPCConcordances="$pathToCPConcordances" > "$marecrdf""$fn".rdf done ;
+
+
 #ls -1 ../data/*.xml | sed 's/\.xml//' | while read i; do saxonb-xslt -t -tree:linked -s "$i".xml -xsl generic.xsl > "$i".rdf; echo "Created $i.rdf"; done
 #for i in MAREC/*/*/*.xml ; do filename=$(basename $i) ; path=${i%/*} ; xmllint --format "$i" > /tmp/"$filename" ; mv /tmp/"$filename" "$path""/""$filename" ; done
 #grep -RE "kind=\"[^\"]*\"" MAREC/*/*/*.xml | perl -pe 's/.*kind=\"([^\"]*)\".*/$1/' | sort -u
