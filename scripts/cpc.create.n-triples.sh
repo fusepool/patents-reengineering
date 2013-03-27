f70301edf7c6c04e260fb565fae102ac49f7b831
Sarven Capadisli <info@csarven.ca>
2013-03-27 14:30:53 +0100

    Added inplace sort

--- scripts/cpc.create.n-triples.sh
+++ scripts/cpc.create.n-triples.sh
@@ -2,4 +2,6 @@
 
 . ./patents.config.sh
 
-for i in "$cpcscheme"*.rdf ; do rapper -g "$i" >> "$cpcscheme"import/cpc.nt ; done
+for i in "$cpcscheme"*.rdf ; do rapper -g "$i" >> "$cpcscheme"import/cpc.nt ; done ;
+
+sort -u "$cpcscheme"import/cpc.nt -o "$cpcscheme"import/cpc.nt ;
--- scripts/ecla.create.n-triples.sh
+++ scripts/ecla.create.n-triples.sh
@@ -3,3 +3,5 @@
 . ./patents.config.sh
 
 for i in "$eclascheme"*.rdf ; do rapper -g "$i" >> "$eclascheme"import/ecla.nt ; done
+
+sort -u "$eclascheme"import/ecla.nt -o "$eclascheme"import/ecla.nt ;
