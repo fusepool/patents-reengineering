28aee30f901ad7d23b046c05a1998c926500be6a
Sarven Capadisli <info@csarven.ca>
2013-03-28 12:23:51 +0100

    Sorting

--- scripts/marec.sh
+++ scripts/marec.sh
@@ -5,7 +5,7 @@
 marec="/data/IREC/EP/000000/00/";
 marecrdf="/data/marec/";
 
-find "$marec" -name *.xml | while read i ; do path=${i%/*} ; file=$(basename $i) ; fn=${file%.*}; saxonb-xslt -ext:on -tree:linked -s "$i" -xsl ../src/main/resources/xsl/marec.xsl pathToCPCConcordances="$pathToCPCConcordances" > "$marecrdf""$fn".rdf ; done
+find "$marec" -name *.xml | sort | while read i ; do path=${i%/*} ; file=$(basename $i) ; fn=${file%.*}; saxonb-xslt -ext:on -tree:linked -s "$i" -xsl ../src/main/resources/xsl/marec.xsl pathToCPCConcordances="$pathToCPCConcordances" > "$marecrdf""$fn".rdf ; done
 
 #ls -1 ../data/*.xml | sed 's/\.xml//' | while read i; do saxonb-xslt -t -tree:linked -s "$i".xml -xsl generic.xsl > "$i".rdf; echo "Created $i.rdf"; done
 #for i in MAREC/*/*/*.xml ; do filename=$(basename $i) ; path=${i%/*} ; xmllint --format "$i" > /tmp/"$filename" ; mv /tmp/"$filename" "$path""/""$filename" ; done
