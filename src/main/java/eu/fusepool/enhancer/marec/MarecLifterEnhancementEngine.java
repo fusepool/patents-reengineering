1c73c1edd209290543aaf1988af7a3d7452e1d65
Luigi Selmi <luigi@luigi.(none)>
2013-05-25 20:11:44 +0200

    Added new method to create entity annotation for each entity found.

--- src/main/java/eu/fusepool/enhancer/marec/MarecLifterEnhancementEngine.java
+++ src/main/java/eu/fusepool/enhancer/marec/MarecLifterEnhancementEngine.java
@@ -15,17 +15,24 @@ import java.util.Set;
 
 import org.apache.clerezza.rdf.core.LiteralFactory;
 import org.apache.clerezza.rdf.core.MGraph;
+import org.apache.clerezza.rdf.core.NonLiteral;
 import org.apache.clerezza.rdf.core.Triple;
+import org.apache.clerezza.rdf.core.TripleCollection;
 import org.apache.clerezza.rdf.core.UriRef;
+import org.apache.clerezza.rdf.core.impl.PlainLiteralImpl;
 import org.apache.clerezza.rdf.core.impl.TripleImpl;
 import org.apache.clerezza.rdf.core.serializedform.Parser;
 import org.apache.clerezza.rdf.core.serializedform.SupportedFormat;
+import org.apache.clerezza.rdf.ontologies.RDF;
+import org.apache.clerezza.rdf.ontologies.FOAF;
+import org.apache.clerezza.rdf.utils.GraphNode;
 import org.apache.commons.io.IOUtils;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
+import org.apache.stanbol.commons.indexedgraph.IndexedGraph;
 import org.apache.stanbol.commons.indexedgraph.IndexedMGraph;
 import org.apache.stanbol.enhancer.contentitem.inmemory.InMemoryContentItemFactory;
 import org.apache.stanbol.enhancer.servicesapi.ContentItem;
@@ -33,10 +40,12 @@ import org.apache.stanbol.enhancer.servicesapi.ContentItemFactory;
 import org.apache.stanbol.enhancer.servicesapi.ContentSink;
 import org.apache.stanbol.enhancer.servicesapi.EngineException;
 import org.apache.stanbol.enhancer.servicesapi.EnhancementEngine;
+import org.apache.stanbol.enhancer.servicesapi.InvalidContentException;
 import org.apache.stanbol.enhancer.servicesapi.ServiceProperties;
 import org.apache.stanbol.enhancer.servicesapi.helper.EnhancementEngineHelper;
 import org.apache.stanbol.enhancer.servicesapi.impl.AbstractEnhancementEngine;
 import org.apache.stanbol.enhancer.servicesapi.rdf.NamespaceEnum;
+import org.apache.stanbol.enhancer.servicesapi.rdf.TechnicalClasses;
 import org.apache.stanbol.entityhub.servicesapi.Entityhub;
 import org.osgi.framework.Constants;
 import org.osgi.framework.InvalidSyntaxException;
@@ -44,7 +53,7 @@ import org.osgi.framework.ServiceReference;
 import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.log.LogService;
-
+import org.wymiwyg.commons.mediatypes.MimeType;
 import eu.fusepool.enhancer.marec.xslt.CatalogBuilder;
 import eu.fusepool.enhancer.marec.xslt.XMLProcessor;
 import eu.fusepool.enhancer.marec.xslt.impl.PatentXSLTProcessor;
@@ -82,14 +91,8 @@ implements EnhancementEngine, ServiceProperties {
 	//private static final Logger log = LoggerFactory.getLogger(MarecLifterEnhancementEngine.class);
 	private TCServiceLocator serviceLocator ;
 	
+	private static final String MIME_TYPE_XML = "application/xml";
 	
-	
-	
-	
-	/*****
-	 * graph uri for the triplestore
-	 */
-	private static final String graphUri = "urn:fusepool-graph1" ;
 	
 	//@SuppressWarnings("unused")
 	public static final boolean DEF_CLEAN = false ;
@@ -113,11 +116,6 @@ implements EnhancementEngine, ServiceProperties {
 		types.add(SupportedFormat.X_TURTLE.toLowerCase());
 		supportedMediaTypes = Collections.unmodifiableSet(types);
 	}
-
-	
-	
-    public static final UriRef FOAF_PERSON = new UriRef(NamespaceEnum.foaf
-            + "person");
 	
 	protected ComponentContext componentContext ;
 
@@ -132,8 +130,6 @@ implements EnhancementEngine, ServiceProperties {
 
 	@Reference
 	protected Parser parser ;
-
-	//protected TripleCollection tripleCollection ;
 	
 
 	//@SuppressWarnings("unchecked")
@@ -155,21 +151,8 @@ implements EnhancementEngine, ServiceProperties {
 			logService.log(LogService.LOG_ERROR, "Error building dtd catalog", e1) ;
 		}
 		
-		try { // TODO: errore nell'attivazione ?
-			serviceLocator = new TCServiceLocator(ce.getBundleContext(), "graph.uri=" + graphUri) ;
-					//"graph.uri=om.go5th.yard.clerezza.01") ;	
-//			if(CLEAN_ON_STARTUP) {
-//				TripleCollection collection = serviceLocator.getTripleCollection() ;
-//				if(collection!=null) {
-//					collection.clear() ;
-//				}
-//			}
-		} catch (InvalidSyntaxException e) {
-			logService.log(LogService.LOG_ERROR,"Invalid graph.uri syntax", e) ;
-		} catch (Exception e) {
-			logService.log(LogService.LOG_ERROR, "Error creating service locator", e) ;
-		}
-		logService.log(LogService.LOG_INFO, "activating "+this.getClass().getName());
+		
+		logService.log(LogService.LOG_INFO, "PatentEngine being activated " + this.getClass().getName());
 
 	}
 
@@ -178,122 +161,134 @@ implements EnhancementEngine, ServiceProperties {
 		catalogBuilder.cleanupFiles() ;
 	}
 
-	@Override
+	/*
+	 * Check if content is present and mime type is correct.
+	 * 
+	 */
 	public int canEnhance(ContentItem ci) throws EngineException {
-		return ENHANCE_SYNCHRONOUS;
+		 
+        try {
+            if ((ci.getBlob() == null)
+                    || (ci.getBlob().getStream().read() == -1)) {
+                return CANNOT_ENHANCE;
+            }
+            
+            if (! ci.getMimeType().equals(this.MIME_TYPE_XML)) {
+            	return CANNOT_ENHANCE;
+            }
+        } catch (IOException e) {
+            logService.log(CANNOT_ENHANCE, "Failed to get the text for "
+                    + "enhancement of content: " + ci.getUri() + " or wrong MIME TYPE (must be application/xml)");
+            throw new InvalidContentException(this, ci, e);
+        }
+        // no reason why we should require to be executed synchronously
+        return ENHANCE_ASYNC;
+        
+        // or there are some reasons ?
+		//return ENHANCE_SYNCHRONOUS;
 	}
 
 	@Override
 	public void computeEnhancements(ContentItem ci) throws EngineException {
 		UriRef contentItemId = ci.getUri();
-		logService.log(LogService.LOG_INFO, "UriRef: "+contentItemId.getUnicodeString()) ;
-
-
-
-		
-
-		//TODO: check if mimetype is supported
-//		String mimeType = ci.getMimeType() ;
-//		if(!isSupported(mimeType)) {
-//			throw new EngineException("Cannot enhance mimetype: "+mimeType) ;
-//		}
-		
-		// Load rdf graph 
-		MGraph rdfGraph = new IndexedMGraph();
+		logService.log(LogService.LOG_INFO, "UriRef: "+contentItemId.getUnicodeString()) ;		
 		
 		try {
 	
 			ci.getLock().writeLock().lock();
-			XMLProcessor processor = new PatentXSLTProcessor() ;
-			InputStream rdfIs = null ;
-			try {
 			
-				rdfIs = processor.processPatentXML(ci.getStream()) ;
-				parser.parse(rdfGraph, rdfIs, SupportedFormat.RDF_XML) ;
-				rdfIs.close() ;
-				
-			} catch (Exception e) {
-				logService.log(LogService.LOG_ERROR, "Wrong data format for the "+this.getName()+" enhancer", e) ;
-				return ;
-			}
-		
-			ci.getMetadata().addAll(rdfGraph) ;
+			// Transform the patent XML file into RDF
+			transformXML(ci);
 			
-			/// 
-			InputStream toCopy = ci.getStream() ;
-			UriRef blobUri = new UriRef("urn:PatentEngine:plain-text:"+randomUUID());
-			ContentSink plainTextSink = ciFactory.createContentSink("text/plain");
-			OutputStream os = plainTextSink.getOutputStream() ;
-			IOUtils.copy(toCopy, os) ;
-			IOUtils.closeQuietly(toCopy) ;
-			IOUtils.closeQuietly(os) ;
-			ci.addPart(blobUri, plainTextSink.getBlob());
+			// Add a part to the content item as a text/plain representation of the XML document 
+			addPartToContentItem(ci);
 			
+			// Add enhancements to the metadata
+			addEnhancements(ci);
 			
-			UriRef enhancementUri = EnhancementEngineHelper.createEntityEnhancement(ci, this) ;
-			MGraph metadata = ci.getMetadata() ;
-			LiteralFactory lf = LiteralFactory.getInstance();
-            // add confidence information
-            metadata.add(new TripleImpl(enhancementUri,
-                    org.apache.stanbol.enhancer.servicesapi.rdf.Properties.ENHANCER_CONFIDENCE, lf
-                            .createTypedLiteral(Double.valueOf(1.0))));
-			
-//            rdfGraph.filter(null, predicate, object) ;
-            
-            
-            
-            
-            /*
-             * first attempt to filter the extracted Triple collection.....
-             */
-            int i = 0 ;
-            Iterator<Triple> entities = rdfGraph.filter(null, org.apache.stanbol.enhancer.servicesapi.rdf.Properties.RDF_TYPE, FOAF_PERSON) ;
-    		for (Triple triple : rdfGraph) {
-    			i++ ;
-    			System.out.println(triple.toString());
-    		}
-            System.out.println(i+ "triples out of "+rdfGraph.size());
-            
-		/*	
-		RdfValueFactory valueFactory = RdfValueFactory.getInstance();
-		Map<String,Representation> representations = new HashMap<String,Representation>();
-		Set<NonLiteral> processed = new HashSet<NonLiteral>();
-		for(Iterator<Triple> st = rdfGraph.iterator();st.hasNext();){
-			Triple curTriple = st.next() ;
-			NonLiteral resource = curTriple.getSubject();
-			if(resource instanceof UriRef){
-				representations.put(((UriRef)resource).getUnicodeString(),
-						valueFactory.createRdfRepresentation((UriRef)resource, rdfGraph));
-			} else {
-				logService.log(LogService.LOG_INFO, "skipped: "+curTriple);
-				logService.log(LogService.LOG_INFO, "not UriRef: "+resource);	
-			}
-		}
-		
-		*/
-		
-		/* stores the triples in a graph	
-		TripleCollection tripleCollection = serviceLocator.getTripleCollection() ;
-		if(tripleCollection!=null) {
-			if(tripleCollection.addAll(rdfGraph)) {
-				logService.log(LogService.LOG_INFO, this.getClass().getName()+" added collection to yard...");
-			} else{
-				logService.log(LogService.LOG_WARNING, this.getClass().getName()+" collection NOT added to yard...");
-			}
-		}
-		*/
-
-		
-		
-		
 			
 		} catch (Exception e) {
 			logService.log(LogService.LOG_ERROR, "", e) ;
 			
-		} finally {
+		} 
+		finally {
 			ci.getLock().writeLock().unlock();
 		}
+		
 	}
+	
+	/*
+	 * Transform patent XML documents into RDF using an XSLT transformation and add the graph to the content item metadata.
+	 */
+	private void transformXML(ContentItem ci) throws EngineException {
+		
+		XMLProcessor processor = new PatentXSLTProcessor() ;
+		InputStream rdfIs = null ; 
+	
+		
+		try {
+			ci.getLock().writeLock().lock();
+			MGraph metadata = new IndexedMGraph();
+			rdfIs = processor.processPatentXML(ci.getStream()) ;
+			parser.parse(metadata, rdfIs, SupportedFormat.RDF_XML) ;
+			rdfIs.close() ;
+			ci.getMetadata().addAll(metadata) ;
+			
+		} catch (Exception e) {
+			logService.log(LogService.LOG_ERROR, "Wrong data format for the " + this.getName() + " enhancer.", e) ;
+			return ;
+		}
+		finally {
+			ci.getLock().writeLock().unlock();
+		}
+		
+	}
+	
+	/*
+	 *  Add a part to the content item as a text/plain representation of the XML document
+	 */
+	private void addPartToContentItem(ContentItem ci) throws EngineException, IOException {
+		ci.getLock().writeLock().lock();
+		InputStream toCopy = ci.getStream() ;
+		UriRef blobUri = new UriRef("urn:patent-engine:plain-text:" + randomUUID());
+		ContentSink plainTextSink = ciFactory.createContentSink("text/plain");
+		OutputStream os = plainTextSink.getOutputStream() ;
+		IOUtils.copy(toCopy, os) ;
+		IOUtils.closeQuietly(toCopy) ;
+		IOUtils.closeQuietly(os) ;
+		ci.addPart(blobUri, plainTextSink.getBlob());
+		
+		ci.getLock().writeLock().unlock();
+		
+	}
+	
+	/*
+	 * Create an enhancement for each entity found by the transformation of the XML document and add them 
+	 * to the content item metadata with properties to refer it to the entity and some more
+	 */
+	private void addEnhancements(ContentItem ci) {
+		ci.getLock().writeLock().lock();
+		if (! ci.getMetadata().isEmpty()) {
+			
+			Iterator<Triple> ipersons = ci.getMetadata().filter(null, RDF.type, FOAF.Person) ;
+			
+			while (ipersons.hasNext()) {
+				// create an entity annotation
+				UriRef entityAnnotation = EnhancementEngineHelper.createEntityEnhancement(ci, this) ;
+				// add a triple to link the enhancement to the entity
+				ci.getMetadata().add( new TripleImpl(entityAnnotation, org.apache.stanbol.enhancer.servicesapi.rdf.Properties.ENHANCER_ENTITY_REFERENCE, ipersons.next().getSubject() ));
+				// add a confidence value
+				ci.getMetadata().add( new TripleImpl(entityAnnotation, org.apache.stanbol.enhancer.servicesapi.rdf.Properties.ENHANCER_CONFIDENCE, new PlainLiteralImpl("1.0") ));
+			 
+			}
+	        
+		}
+		
+		ci.getLock().writeLock().unlock();
+		
+	}
+	
+	
 
 	@Override
 	public Map<String, Object> getServiceProperties() {
