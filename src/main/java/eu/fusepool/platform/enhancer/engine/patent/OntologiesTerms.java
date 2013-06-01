a6d6972b7b25d71e0ebed4694fdd8fbb2a0aca25
Luigi Selmi <selmi_luigi@hotmail.com>
2013-06-01 16:53:22 +0200

    removed uriref of FISE terms available from TechnicalClasses

--- src/main/java/eu/fusepool/platform/enhancer/engine/patent/OntologiesTerms.java
+++ src/main/java/eu/fusepool/platform/enhancer/engine/patent/OntologiesTerms.java
@@ -8,11 +8,6 @@ import org.apache.clerezza.rdf.core.UriRef;
  */
 public class OntologiesTerms {
 	
-	// FISE
-	final static String FISE_NS =  "http://fise.iks-project.eu/ontology/";
-	final static UriRef fiseEntityReference = new UriRef(FISE_NS + "entity-reference");
-	final static UriRef fiseConfidence = new UriRef(FISE_NS + "confidence");
-
 	// Patent Metadata Ontology
 	final static String PMO_NS = "http://www.patexpert.org/ontologies/pmo.owl#";
 	final static UriRef pmoPatentPublication = new UriRef(PMO_NS + "PatentPublication");
