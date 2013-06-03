5419c2ae5d90a77750ce368995b8bdad7a72a4ef
Luigi Selmi <selmi_luigi@hotmail.com>
2013-06-03 17:30:35 +0200

    updated some ontological terms

--- src/main/java/eu/fusepool/platform/enhancer/engine/patent/OntologiesTerms.java
+++ src/main/java/eu/fusepool/platform/enhancer/engine/patent/OntologiesTerms.java
@@ -8,6 +8,10 @@ import org.apache.clerezza.rdf.core.UriRef;
  */
 public class OntologiesTerms {
 	
+	// FISE
+	final static String FISE_NS = "http://fise.iks-project.eu/ontology/";
+	final static UriRef fiseEntityReference = new UriRef(FISE_NS + "entity-reference");
+	
 	// Patent Metadata Ontology
 	final static String PMO_NS = "http://www.patexpert.org/ontologies/pmo.owl#";
 	final static UriRef pmoPatentPublication = new UriRef(PMO_NS + "PatentPublication");
