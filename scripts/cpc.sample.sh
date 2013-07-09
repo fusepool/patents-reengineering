0abb266745db6a022a3871d5553a508aa5b175ad
Sarven Capadisli <info@csarven.ca>
2013-07-09 17:34:56 +0200

    Turns extensions on

--- scripts/cpc.sample.sh
+++ scripts/cpc.sample.sh
@@ -2,7 +2,7 @@
 cpcscheme="../data/cpc-scheme/";
 xslDocument="../src/main/resources/xsl/cpc.xsl";
 
-for i in "$cpcscheme"*.xml ; do file=$(basename "$i"); fn=${file%.*}; saxonb-xslt -t -tree:linked -s "$i" -xsl "$xslDocument" xmlDocument="$i" > "$cpcscheme""$fn".rdf ; done
+for i in "$cpcscheme"*.xml ; do file=$(basename "$i"); fn=${file%.*}; saxonb-xslt -ext:on -t -tree:linked -s "$i" -xsl "$xslDocument" xmlDocument="$i" > "$cpcscheme""$fn".rdf ; done
 
 #saxonb-xslt -tree:linked -s ../data/ecla/ecla-A.xml -xsl ../xsl/ecla.xsl > ../data/ecla/ecla-A.rdf
 #saxonb-xslt -tree:linked -s ../data/ecla/ecla-A01B.xml -xsl ../xsl/ecla.xsl > ../data/ecla/ecla-A01B.rdf
--- scripts/cpc.sh
+++ scripts/cpc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . ./patents.config.sh
 
-for i in "$cpcscheme"*.xml ; do file=$(basename "$i"); fn=${file%.*}; saxonb-xslt -t -tree:linked -s "$i" -xsl "$xslCPC" xmlDocument="$i" > "$cpcscheme""$fn".rdf ; done
+for i in "$cpcscheme"*.xml ; do file=$(basename "$i"); fn=${file%.*}; saxonb-xslt -ext:on -t -tree:linked -s "$i" -xsl "$xslCPC" xmlDocument="$i" > "$cpcscheme""$fn".rdf ; done
