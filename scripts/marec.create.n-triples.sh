b31cf7fcadb36b3cb66a5c4ab76f929710d6b692
Sarven Capadisli <info@csarven.ca>
2013-07-23 16:51:24 +0200

    Minor

--- scripts/marec.create.n-triples.sh
+++ scripts/marec.create.n-triples.sh
@@ -2,7 +2,6 @@
 
 . ./patents.config.sh
 
-marec="../data/marec/";
 find "$marec" -name *.rdf | while read i ; do rapper -g "$i" >> "$marec"import/marec.nt ; done ;
 
 sort -u "$marec"import/marec.nt -o "$marec"import/marec.nt ;
